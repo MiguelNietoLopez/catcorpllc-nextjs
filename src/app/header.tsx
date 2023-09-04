07984029e97e6ebcde53838106fe770c4dfd2664
Miguel Nieto Lopez <miguelnietolopez2002@gmail.com>
2023-09-04 00:24:33 -0500

    Daily Work

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,37 +1,26 @@
 "use client"
 import Link from "next/link";
-import { type } from "os";
-import { use, useReducer, useState } from "react";
+import ccllc from "../../public/images/CCLLC.svg"
+import Image from "next/image";
 
 export default function Header() {
-    const [mouseX, setMouseX] = useState(-1);
-    const [mouseY, setMouseY] = useState(-1);
-    const [highlightDisplay, sethighlightDisplay] = useState("hidden");
-    const mouseHandle = (e: React.MouseEvent) => { 
-        setMouseX(e.clientX);
-        setMouseY(e.clientY);
-    }
-    const mouseOver = (e: React.MouseEvent) => {
-        sethighlightDisplay("absolute");
-    }
-    const mouseOut = (e: React.MouseEvent) => {
-        sethighlightDisplay("hidden");
-    }
     return (
         <>
-        <nav id="header" className="fixed w-full z-2 h-20 text-3xl flex flex-nowrap bg-[rgba(106,120,101,0.9)] " onMouseMove={(e) => {mouseHandle(e)}} onMouseOver={(e) => {mouseOver(e)}} onMouseOut={(e) => {mouseOut(e)}}>
-            <div id="headerTitleDiv" className="flex-1 flex w-auto">
-                <div id="headerimg" className="w-auto"></div>
+        <nav id="header" className="fixed w-full z-2 h-20 text-3xl flex flex-nowrap bg-[rgba(106,120,101,0.9)] ">
+            <div id="headerTitleDiv" className="flex-1 static flex w-auto">
+                <div id="headerimg" className="w-auto">
+                    <Image className="h-20 w-20 ml-2 mr-5" src={ccllc} alt="test"></Image>
+                </div>
                 <div id="headerTitle" className="flex-1 w-auto">Cat Corp LLC</div>
             </div>
-            <div id="headerListDiv" className="flex-1 z-2 flex w-auto">
+            <div id="headerListDiv" className="flex-1 static flex w-auto">
             <Link className="item flex-1 hover:bg-[rgba(106,120,101,1)]" href="/">Home</Link>
             <Link className="item flex-1 hover:bg-[rgba(106,120,101,1)]" href="/about">About</Link>
             <Link className="item flex-1 hover:bg-[rgba(106,120,101,1)]" href="/solutions">Solutions</Link>
             <Link className="item flex-1 hover:bg-[rgba(106,120,101,1)]" href="/contact">Contact</Link>
             </div>
-            <div id="headerhighlight" className={"z-4 h-5 w-5 m-0 p-0 block "+ highlightDisplay +" bg-red-500 "} style={{transform: "translate("+ mouseX + "px," + mouseY +"px)"}}></div>
         </nav>
+
         <div id="emptyspacer" className="w-full z-0 h-20"></div>
         </>
     )
