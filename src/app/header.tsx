e30e4e120e25fb3c8f074b4b718488633d7a4fd6
Miguel Nieto Lopez <miguelnietolopez2002@gmail.com>
2023-09-04 22:57:37 -0500

    header complete

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -6,18 +6,18 @@ import Image from "next/image";
 export default function Header() {
     return (
         <>
-        <nav id="header" className="fixed w-full z-2 h-20 text-3xl flex flex-nowrap bg-[rgba(106,120,101,0.9)] ">
-            <div id="headerTitleDiv" className="flex-1 static flex w-auto">
-                <div id="headerimg" className="w-auto">
-                    <Image className="h-20 w-20 ml-2 mr-5" src={ccllc} alt="test"></Image>
+        <nav id="header" className="fixed w-full items-stretch z-2 h-20 text-3xl flex flex-nowrap bg-[rgba(106,120,101,0.9)] ">
+            <div id="headerTitleDiv" className="flex-1 static leading-[100%] flex w-auto">
+                <div id="headerimg" className="align-middle w-auto">
+                    <Image className="h-20 w-20 ml-2 mr-5 " src={ccllc} alt="test"></Image>
                 </div>
-                <div id="headerTitle" className="flex-1 w-auto">Cat Corp LLC</div>
+                <div id="headerTitle" className="flex-1 flex items-center w-auto">Cat Corp LLC</div>
             </div>
-            <div id="headerListDiv" className="flex-1 static flex w-auto">
-            <Link className="item flex-1 hover:bg-[rgba(106,120,101,1)]" href="/">Home</Link>
-            <Link className="item flex-1 hover:bg-[rgba(106,120,101,1)]" href="/about">About</Link>
-            <Link className="item flex-1 hover:bg-[rgba(106,120,101,1)]" href="/solutions">Solutions</Link>
-            <Link className="item flex-1 hover:bg-[rgba(106,120,101,1)]" href="/contact">Contact</Link>
+            <div id="headerListDiv" className="flex-1 items-stretch  text-center static flex w-auto">
+            <Link className="item flex-1 flex items-center justify-center hover:bg-[rgba(106,120,101,1)]" href="/">Home</Link>
+            <Link className="item flex-1 flex items-center justify-center hover:bg-[rgba(106,120,101,1)]" href="/about">About</Link>
+            <Link className="item flex-1 flex items-center justify-center hover:bg-[rgba(106,120,101,1)]" href="/solutions">Solutions</Link>
+            <Link className="item flex-1 flex items-center justify-center hover:bg-[rgba(106,120,101,1)]" href="/contact">Contact</Link>
             </div>
         </nav>
 
